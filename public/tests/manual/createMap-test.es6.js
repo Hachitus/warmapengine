9c1951ae22485627e22d577e9241f08efae5e09e
Hachitus <janne.hyytia@saunalahti.fi>
2015-07-31 21:43:31 +0300

    Getting the project git ready for more reasonable development.

--- public/tests/manual/createMap-test.es6.js
+++ public/tests/manual/createMap-test.es6.js
@@ -1,24 +1,24 @@
 'use strict';
-
 /* ====== Library imports ====== */
 
 /* ====== Own module imports ====== */
 //var Map = require( '../public/components/map/Map');
 import { createMap } from '../../components/factories/horizontalHexaFactory';
-/* Read data from files, to use with testing */
-import { gameData } from '../../tests/data/gameData';
-import { typeData } from '../../tests/data/typeData';
-import { mapData } from '../../tests/data/mapData';
-import { preload } from '../../components/preloading/preloading';
 
 /* ===== Import plugins ===== */
 import { map_drag } from "../../components/map/core/move/map_drag";
 import { map_zoom } from '../../components/map/core/zoom/map_zoom';
 import { object_select_hexagon } from '../../components/map/hexagons/object_select/object_select_hexagon';
 
-var map;
+/* DATA FILES used for testing */
+import { gameData } from '../../tests/data/gameData';
+import { typeData } from '../../tests/data/typeData';
+import { mapData } from '../../tests/data/mapData';
+import { preload } from '../../components/preloading/preloading';
+
 window.initMap = function () {
   var canvasElement = document.getElementById("mapCanvas");
+  var map;
 
   map = createMap(canvasElement, gameData, mapData, typeData);
 
@@ -37,6 +37,8 @@ window.initMap = function () {
       console.log("preloading complete? Map should be ready to init?");
       map.init( [ map_zoom, map_drag, object_select_hexagon ], { x: 41, y: 47 }, undefined );
     });
+
+  return map;
 
     /* ====== private functions, or to be moved elsewhere ====== */
   function preloadErrorHandler(err) {

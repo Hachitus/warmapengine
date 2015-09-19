9f75a58a8a5f24cfa3121e24da6d2f2cd8b1bc2c
Hachitus <janne.hyytia@saunalahti.fi>
2015-09-19 23:16:59 +0300

    corrected gitIgnore a bit

---
+++ appModules/basePath.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export var basePath = (function() {
+	var base = "/var/www/warmapengine/";
+	var public = "/var/www/warmapengine/public";
+	
+	return {
+		base: base,
+		public: public
+	};
+})();

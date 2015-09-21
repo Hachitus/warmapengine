1ed96bd7fd655ddbd82a091bc1d1b6b5507342e9
Hachitus <janne.hyytia@saunalahti.fi>
2015-09-21 20:59:52 +0300

    tidying Map_layer and fixed missing updateTransform

--- public/components/map/core/pixi_Map_layer.js
+++ public/components/map/core/pixi_Map_layer.js
@@ -5,10 +5,6 @@
 @require the PIXI framework in global namespace
 */
 
-/**
- * @todo this.preventSelection. This should determine wether this stage holds data that can be selected by the player
- */
-
 var _UIObjects = [];
 
 /* ===== EXPORT ===== */
@@ -31,67 +27,8 @@ export class Map_layer extends PIXI.Container {
     this.zoomable = false;
     this.preventSelection = false;
   }
-  /** setter and getter
-   * @param {Boolean} status If provided sets the caching status otherwise returns the current status */
-  cacheEnabled(status) {
-    if(status !== undefined) {
-      this._cacheEnabled = status;
-    }
-
-    return this._cacheEnabled;
-  }
-  /** Move layer
-   * @param {x: Number, y: Number} coordinates The amount of x and y coordinates we want the layer to move. I.e.
-   { x: 5, y: 0 }
-   @return this layer instance */
-  move(coordinates) {
-    if (this.movable) {
-      this.x += coordinates.x;
-      this.y += coordinates.y;
-      this.drawThisChild = true;
-    }
-
-    return this;
-  }
-  getChildNamed(name) {
-    if (this.children[0] instanceof PIXI.DisplayObjectContainer ) {
-      for (let child of this.children) {
-        if (child.name.toLowerCase() === name.toLowerCase()) {
-          return child;
-        }
-      }
-    }
-    return false;
-  }
-  setScale(amount) {
-    return this.scale.x = this.scale.y = amount;
-  }
-  getScale() {
-    return this.scale.x;
-  }
-  getUIObjects() {
-    return _UIObjects;
-  }
-  emptyUIObjects() {
-    _UIObjects.map(obj => {
-      this.removeChild(obj);
-      obj = null;
-    });
-
-    return _UIObjects;
-  }
-  addUIObjects(objects) {
-    _UIObjects = _UIObjects || [];
-    if(Array.isArray(objects)) {
-      this.addChild.apply(this, objects);
-    } else {
-      this.addChild( objects );
-    }
-    _UIObjects.push( objects );
-
-    return _UIObjects;
-  }
 }
+Object.assign(Map_layer.prototype, _getBaseContainerClass());
 
 /* REMEMBER! PIXI.ParticleContainer has limited support for features (like filters etc.), at some point you have to use
 normal container too, but since this one is optimized for performance we use it here first */
@@ -114,74 +51,8 @@ export class Map_spriteLayer extends PIXI.ParticleContainer {
     this.movable = true;
     this.zoomable = false;
     this.preventSelection = false;
-    /* createjs / super properties. Used also for controlling and optimizing the engine */
-    this.visible = true;
-    this.tickEnabled = false;
-    this.tickChildren = false;
-    this.mouseChildren = false;
-    this.mouseEnabled = false;
   }
-  /** setter and getter
-   * @param {Boolean} status If provided sets the caching status otherwise returns the current status */
-  cacheEnabled(status) {
-    if(status !== undefined) {
-      this._cacheEnabled = status;
-    }
-
-    return this._cacheEnabled;
-  }
-  /** Move layer
-   * @param {x: Number, y: Number} coordinates The amount of x and y coordinates we want the layer to move. I.e.
-   { x: 5, y: 0 }
-   @return this layer instance */
-  move(coordinates) {
-    if (this.movable) {
-      this.x += coordinates.x;
-      this.y += coordinates.y;
-      this.drawThisChild = true;
-    }
-
-    return this;
-  }
-  getChildNamed(name) {
-    if (this.children[0] instanceof PIXI.DisplayObjectContainer ) {
-      for (let child of this.children) {
-        if (child.name.toLowerCase() === name.toLowerCase()) {
-          return child;
-        }
-      }
-    }
-    return false;
-  }
-  setScale(amount) {
-    return this.scale.x = this.scale.y = amount;
-  }
-  getScale() {
-    return this.scale.x;
-  }
-  getUIObjects() {
-    return _UIObjects;
-  }
-  emptyUIObjects() {
-    _UIObjects.map(obj => {
-      this.removeChild(obj);
-      obj = null;
-    });
-
-    return _UIObjects;
-  }
-  addUIObjects(objects) {
-    _UIObjects = _UIObjects || [];
-    if(Array.isArray(objects)) {
-      this.addChild.apply(this, objects);
-    } else {
-      this.addChild( objects );
-    }
-    _UIObjects.push( objects );
-
-    return _UIObjects;
-  }
-	/* If we want the interactive manager to work correctly for detecting coordinate clicks, we need correct worldTransform data, for
+		/* If we want the interactive manager to work correctly for detecting coordinate clicks, we need correct worldTransform data, for
 	the children too. I think this has been normally disabled to make the particleContainer as efficient as possible */
 	updateTransform() {
 		if (!this.visible)
@@ -195,4 +66,73 @@ export class Map_spriteLayer extends PIXI.ParticleContainer {
 				this.children[i].updateTransform();
 		}
 	}
+}
+Object.assign(Map_spriteLayer.prototype, _getBaseContainerClass());
+
+function _getBaseContainerClass() {
+	return {
+		setCache(status) {
+			if(status) {
+				this._cacheEnabled = true;
+			} else {
+				this._cacheEnabled = false;
+			}
+
+			return this._cacheEnabled;
+		},
+		getCache(status) {
+			return this._cacheEnabled;
+		},
+		/** Move layer
+		 * @param {x: Number, y: Number} coordinates The amount of x and y coordinates we want the layer to move. I.e.
+		 { x: 5, y: 0 }
+		 @return this layer instance */
+		move(coordinates) {
+			if (this.movable) {
+				this.x += coordinates.x;
+				this.y += coordinates.y;
+				this.drawThisChild = true;
+			}
+
+			return this;
+		},
+		getChildNamed(name) {
+			if (this.children[0] instanceof PIXI.DisplayObjectContainer ) {
+				for (let child of this.children) {
+					if (child.name.toLowerCase() === name.toLowerCase()) {
+						return child;
+					}
+				}
+			}
+			return false;
+		},
+		setScale(amount) {
+			return this.scale.x = this.scale.y = amount;
+		},
+		getScale() {
+			return this.scale.x;
+		},
+		getUIObjects() {
+			return _UIObjects;
+		},
+		emptyUIObjects() {
+			_UIObjects.map(obj => {
+				this.removeChild(obj);
+				obj = null;
+			});
+
+			return _UIObjects;
+		},
+		addUIObjects(objects) {
+			_UIObjects = _UIObjects || [];
+			if(Array.isArray(objects)) {
+				this.addChild.apply(this, objects);
+			} else {
+				this.addChild( objects );
+			}
+			_UIObjects.push( objects );
+
+			return _UIObjects;
+		}
+	};
 }

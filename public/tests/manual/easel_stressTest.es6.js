5e0c84a80cba191501a577ce61a6c561d5c6aad5
Janne Hyytiä <janne.hyytia@saunalahti.fi>
2015-10-11 21:22:24 +0300

    easel stress test also takes options

---
+++ public/tests/manual/easel_stressTest.es6.js
@@ -0,0 +1,227 @@
+/* global PIXI, System, alert, console */
+
+'use strict';
+/* ====== Library imports ====== */
+
+/* ====== Own module imports ====== */
+//var Map = require( '../public/components/map/Map');
+/* THIS POLYFILL IS NEEDED FOR IE11, maybe Symbol os something missing: http://babeljs.io/docs/usage/polyfill/ */
+//require("babel/polyfill");
+
+import { createMap } from '../../components/factories/horizontalHexaFactory';
+
+/* DATA FILES used for testing */
+import { gameData } from '../../tests/data/gameData';
+import { typeData } from '../../tests/data/typeData';
+import { Preload } from '../../components/preloading/preloading';
+
+import { environmentDetection } from '../../components/map/core/utils/utils';
+if (typeof Hammer === 'undefined' && environmentDetection.isMobile_detectUserAgent()) {
+  alert("You seem to be using mobile device, I suggest you use mobile site for tests, since this won't work for you");
+}
+
+/** ===== CONFIGS ===== */
+const HEXASIZE = {
+  x: 41,
+  y: 47
+};
+/* Note the y is 3/4 of the actual height */
+const HEXAGON_DISTANCES = {
+  x: 82,
+  y: 94 * 0.75
+};
+
+/* Do the map: */
+var mapsize;
+window.getMapData = getMapData;
+window.initMap = initMap;
+
+/**************************************
+****** GENERATE RANDOM MAP DATA *******
+**************************************/
+function getMapData(mapsize) {
+  var data = {
+    gameID: "53837d47976fed3b24000005",
+    turn: 1,
+    startPoint: { x: 0, y: 0 },
+    element: "#mapCanvas",
+    layers: []
+  };
+  var unitCount = 10000;
+  var terrainTypeCount = 4;
+  var unitTypeCount = 56;
+  var objGroup, layerData;
+  var coordMapsize = {
+    x: mapsize,
+    y: mapsize
+  }
+
+  return {
+    gameID: "53837d47976fed3b24000005",
+    turn: 1,
+    startPoint: { x: 0, y: 0 },
+    element: "#mapCanvas",
+    layers: [
+    populateTerrainLayer(HEXAGON_DISTANCES, terrainTypeCount, coordMapsize),
+    populateUnitLayer(HEXAGON_DISTANCES, unitTypeCount, coordMapsize)
+    ]
+  };
+}
+
+/******** FUNCTIONS *********/
+function initMap(mapData, options) {
+  var canvasElement = document.getElementById("mapCanvas");
+  var map = {};
+  var globalMap = {
+    data: {}
+  };
+  var preload;
+
+  /* Determines how much stuff we show on screen for stress testing */
+  mapsize = {
+    x: options.mapsize || 1000,
+    y: options.mapsize || 1000
+  }
+
+  preload = new Preload( "", { crossOrigin: false } );
+  preload.add( typeData.graphicData.terrainBase.json );
+  preload.add( typeData.graphicData.unit.json );
+
+  preload.setErrorHandler(function(e) {
+    console.log("preloader error:", e);
+  });
+  preload.setProgressHandler(function(progress) {
+    console.log("progressing" + progress);
+  });
+
+  preload.resolveOnComplete().then(onComplete);
+
+  function onComplete() {
+    var promises = [];
+
+    gameData.mapSize = mapsize;
+
+    map = globalMap.data = createMap(canvasElement, { game: gameData, map: mapData, type: typeData });
+
+    gameData.pluginsToActivate.map.map(plugin => {
+      promises.push(System.import(plugin));
+    });
+
+    Promise.all(promises).then(activetablePlugins => {
+      map.init( activetablePlugins, gameData.mapSize, undefined );
+      if (options.cache) {
+        map.setCache(true);
+      }
+    });
+  }
+
+  return globalMap;
+
+  /* ====== private functions, or to be moved elsewhere ====== */
+  function preloadErrorHandler(err) {
+    console.log("PRELOADER ERROR", err );
+  }
+}
+
+/************ PRIVATE ************/
+function addBase_spriteLayerData(name, group, options = { interactive: true, cache: true }) {
+  var { interactive, cache } = options;
+
+  return {
+    type: "Map_spriteLayer",
+    coord: { x: 0, y: 0 },
+    name: name,
+    group: group, // For quadTrees
+    specials: [{
+      interactive: interactive
+    }],
+    options: {
+      cache: cache
+    },
+    objectGroups: []
+  };
+}
+
+function populateTerrainLayer(size, typeCount, mapsize) {
+  let layerData = addBase_spriteLayerData("terrainLayer", "terrain");
+
+  for (let y = 0; y < mapsize.y; y += size.y ) {
+    let x = 0;
+
+    if (y / size.y % 2 === 0) {
+      x += size.x / 2;
+    }
+
+    while ( x < mapsize.x ) {
+      let realX = x;
+
+      layerData.objectGroups.push({
+        type: "Object_terrain",
+        name: "Terrain", // For quadTrees and debugging
+        typeImageData: "terrainBase",
+        objects: [{
+          objType: Math.floor(Math.random() * typeCount),
+          name:"random_" + Math.random(),
+          _id: Math.random(),
+          coord:{
+            x: realX,
+            y: y
+          },
+          data: {},
+          lastSeenTurn:Math.floor(Math.random() * 10)
+        }]
+      });
+
+      x += size.x;
+    }
+  }
+
+  return layerData;
+}
+
+function populateUnitLayer(size, typeCount, mapsize) {
+  let layerData = addBase_spriteLayerData("unitLayer", "unit");
+  var randomCoords;
+
+  for (let y = 0; y < mapsize.y; y += size.y ) {
+    let x = 0;
+
+    if (y / size.y % 2 === 0) {
+      x += size.x / 2;
+    }
+
+    while ( x < mapsize.x ) {
+      let realX = x;
+
+      layerData.objectGroups.push({
+        type: "Object_unit",
+        name: "Unit", // For quadTrees and debugging
+        typeImageData: "unit",
+        objects: [{
+          objType: Math.floor(Math.random() * typeCount),
+          name: "random_" + Math.random(),
+          _id: Math.random(),
+          coord:{
+            x: realX,
+            y: y
+          },
+          data: {
+            playerID: Math.floor(Math.random() * 10),
+            hp: Math.floor(Math.random() * 100),
+            someStuff: "jalajajajajaja" + Math.random(),
+            someStuff2: "jalajajajajaja" + Math.random(),
+            someStuff3: "jalajajajajaja" + Math.random(),
+            someStuff4: "jalajajajajaja" + Math.random(),
+            someStuff5: "jalajajajajaja" + Math.random(),
+            someStuff6: ("jalajajajajaja" + Math.random()).repeat(30)
+          },
+          lastSeenTurn:Math.floor(Math.random() * 10)
+        }]
+      });
+
+      x += size.x;
+    }
+  }
+
+  return layerData;
+}

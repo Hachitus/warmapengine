8416bd33f68bd516bde8e8d09b29a737e0598f2f
Janne Hyytiä <janne.hyytia@saunalahti.fi>
2015-12-19 21:27:16 +0200

    .

--- public/components/map/core/zoom/map_zoom.js
+++ public/components/map/core/zoom/map_zoom.js
@@ -9,7 +9,6 @@
 
 /** ===== OWN imports ===== */
 import { resizeUtils } from "/components/map/core/utils/utils";
-import { environmentDetection } from "/components/utilities/environment";
 import { eventListeners as eventListenerMod } from '/components/map/core/eventlisteners';
 
 /***********************
@@ -28,7 +27,9 @@ export let map_zoom = setupMap_zoom();
 ******** PUBLIC ********
 ***********************/
 function setupMap_zoom() {
-  var eventListenerCB;
+  var initialized = false;
+  var difference = {};
+  var map;
   /**
    * Maximum and minimum amount, the player can zoom the map
    * @type { farther: Number, closer: Number }
@@ -58,20 +59,15 @@ function setupMap_zoom() {
    * @todo think through should setZoomLimits and setZoomModifier be in map.prototype?
    * But zoomLimit and modifier need to be setable in creation, init or later with setters
    * */
-  function init(map) {
+  function init(thisMap) {
+    map = thisMap;
     map.setPrototype("zoomIn", zoomIn);
     map.setPrototype("zoomOut", zoomOut);
     map.setPrototype("setZoomLimits", setZoomLimits);
     map.setPrototype("setZoomModifier", setZoomModifier);
 
-    if (environmentDetection.isMobile() === "mobile" || environmentDetection.isMobile_detectUserAgent()) {
-      eventListenerCB = _setupZoomEvent_mobile(map);
-    } else {
-      eventListenerCB = _setupZoomEvent(map);
-    }
-
     /* Singleton should have been instantiated before, we only retrieve it with 0 params */
-    eventListenerMod().toggleZoomListener(eventListenerCB);
+    eventListenerMod().toggleZoomListener(eventList);
   }
 
   /****************************************
@@ -125,89 +121,83 @@ function setupMap_zoom() {
   /**********************************
   ******* plugin Initializers *******
   **********************************/
+  function eventList(e, delta, deltaX, deltaY) {
+    if (delta) {
+      handleZoomEventDesktop(e, delta, deltaX, deltaY);
+    } else {
+      eventListenerCBMobile(e);
+    }
+  }
   /**
    * Setup desktop zoomEvent by currying. Internally: Sets up correct scale + moves map accordingly to zoom to the
    * current center coordinates
    *
    * @param  {Map} map             Map instance
    */
-  function _setupZoomEvent(map) {
-    return function handleZoomEvent(e, delta, deltaX, deltaY) {
-      var mouseWheelDelta = deltaY;
-      /* Scale changes when the map is drawn. We make calculations with the old scale before draw */
-      var oldScale = map.getScale();
+  function handleZoomEventDesktop(e, delta, deltaX, deltaY) {
+    var mouseWheelDelta = deltaY;
+    /* Scale changes when the map is drawn. We make calculations with the old scale before draw */
+    var oldScale = map.getScale();
 
-      /* No nasty scrolling side-effects */
-      e.preventDefault();
+    /* No nasty scrolling side-effects */
+    e.preventDefault();
 
-      if (mouseWheelDelta > 0) {
-        if (map.zoomIn()) {
-          map.moveMap(_calculateCenterMoveCoordinates(oldScale, true));
-        }
-      } else if (mouseWheelDelta < 0) {
-        if (map.zoomOut()) {
-          map.moveMap(_calculateCenterMoveCoordinates(oldScale));
-        }
+    if (mouseWheelDelta > 0) {
+      if (map.zoomIn()) {
+        map.moveMap(_calculateCenterMoveCoordinates(oldScale, true));
       }
-    };
+    } else if (mouseWheelDelta < 0) {
+      if (map.zoomOut()) {
+        map.moveMap(_calculateCenterMoveCoordinates(oldScale));
+      }
+    }
   }
-  /**
-   * Setup mobile zoomEvent by currying. Internally: Sets up correct scale and moves map to where the use pinches
-   *
-   * @param  {Map} map             Map instance
-   */
-  function _setupZoomEvent_mobile(map) {
-    var initialized = false;
-    var difference = {};
-
+  function handleZoomEventMobile(e) {
     zoomModifier = zoomModifier * 0.5;
+    var pointers = e.pointers;
+    var coords = [{
+        x: pointers[0].pageX,
+        y: pointers[0].pageY
+      },{
+        x: pointers[1].pageX,
+        y: pointers[1].pageY
+      }];
+    var changeX = Math.abs( coords[0].x - coords[1].x );
+    var changeY = Math.abs( coords[0].y - coords[1].y );
 
-    return function handleZoomEvent_mobile(e) {
-      var pointers = e.pointers;
-      var coords = [{
-          x: pointers[0].pageX,
-          y: pointers[0].pageY
-        },{
-          x: pointers[1].pageX,
-          y: pointers[1].pageY
-        }];
-      var changeX = Math.abs( coords[0].x - coords[1].x );
-      var changeY = Math.abs( coords[0].y - coords[1].y );
+    e.preventDefault();
 
-      e.preventDefault();
-
-      try {
-        if (!initialized) {
-          difference = {
-            x: changeX,
-            y: changeY
-          };
-          initialized = true;
-
-          return;
-        } else if (e.isFinal === true) {
-          initialized = false;
-        }
-
-        if (difference.x + difference.y < changeX + changeY) {
-          if (map.zoomIn()) {
-            map.moveMap(_calculateCenterMoveCoordinates(map.getScale(), true));
-          }
-        } else {
-          if (map.zoomOut()) {
-            map.moveMap(_calculateCenterMoveCoordinates(map.getScale()));
-          }
-        }
-
+    try {
+      if (!initialized) {
         difference = {
           x: changeX,
           y: changeY
         };
+        initialized = true;
 
-      } catch (ev) {
-        console.log("Error! ", ev);
+        return;
+      } else if (e.isFinal === true) {
+        initialized = false;
       }
-    };
+
+      if (difference.x + difference.y < changeX + changeY) {
+        if (map.zoomIn()) {
+          map.moveMap(_calculateCenterMoveCoordinates(map.getScale(), true));
+        }
+      } else {
+        if (map.zoomOut()) {
+          map.moveMap(_calculateCenterMoveCoordinates(map.getScale()));
+        }
+      }
+
+      difference = {
+        x: changeX,
+        y: changeY
+      };
+
+    } catch (ev) {
+      console.log("Error! ", ev);
+    }
   }
 
   /* =================

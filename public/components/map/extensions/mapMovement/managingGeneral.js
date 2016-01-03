8e9a5b5eaaf256e817af51fba53d3d2b87a69e42
Janne Hyytiä <janne.hyytia@saunalahti.fi>
2016-01-03 08:23:22 +0200

    changed managingGeneral folder place

---
+++ public/components/map/extensions/mapMovement/managingGeneral.js
@@ -0,0 +1,193 @@
+/* global console */
+'use strict';
+
+/**
+ * This module manages visibility of the objects, based on are they visible to the player (on the canvas / webgl) or
+ * outside of it. This makes the map a lot faster and reliable resource-wise and lags otherwise.
+ *
+ * @todo  We can make this a bit more efficient still at least should work with static / fixed grid. Now we use
+ * quadtree and that is not optimal
+ */
+
+/***********************
+******* IMPORTS ********
+***********************/
+import { mapEvents } from '/components/map/core/mapEvents';
+import { arrays } from '/components/utilities/general';
+
+/***********************
+********* API **********
+***********************/
+export var managingTileMapMovement = setupManagingTileMapMovement();
+var viewportWorker = new Worker("/components/map/extensions/dynamicMaps/managingTileMapMovement/managingGeneralWorker.js");
+
+/***********************
+******* PUBLIC *********
+***********************/
+function setupManagingTileMapMovement () {
+  const VIEWPORT_OFFSET = 0.15;
+  const CHECK_INTERVAL = 20;
+  var queue = {};
+  var changedCoordinates = {
+    width: 0,
+    height: 0
+  };
+
+  return {
+    addAll,
+    check,
+    startEventListeners
+  };
+
+  function addAll(map) {
+    var scale = map.getScale();
+    var viewportArea, movableLayer;
+
+    movableLayer = map.getMovableLayer();
+    viewportArea = map.getViewportArea();
+    Object.assign( viewportArea, getViewportsRightSideCoordinates(viewportArea) );
+    Object.assign( viewportArea , applyScaleToViewport(viewportArea, map.getScale()) );
+
+    movableLayer.getPrimaryLayers().forEach( layer => {
+      layer.getObjects().forEach(object => {
+        object.visible = isObjectOutsideViewport(object, viewportArea, false, scale) ? false : true;
+      });
+    });
+  }
+  /**
+   * This one checks the that the objects that should currently be visible in the viewport area are visible and outside
+   * of the viewport objects are set .visible = false. This affect performance a lot. Basically when the map moves, we
+   * set a check in the future based on the given intervalCheck milliseconds. And immediately after it we check if there
+   * is another map movement. If there is we set another timeout. This works better with timeouts.
+   *
+   * NOTE! This uses webWorkers. They seemed to speed up the check, when timing with performance.now.
+   *
+   * @param  {[type]} layer [description]
+   * @param  {[type]} map   [description]
+   * @return {[type]}       [description]
+   */
+  function check(map) {
+    if (queue.processing) {
+      return false;
+    }
+    queue.processing = true;
+
+    let viewportFn = setupHandleViewportArea(queue, map, changedCoordinates);
+    window.setTimeout(viewportFn, CHECK_INTERVAL);
+
+    function setupHandleViewportArea(queue, map, changedCoordinates) {
+      var viewportArea = map.getViewportArea();
+
+      try {
+        if (window.Worker) {
+          viewportWorker.onmessage = function(e) {
+            try {
+              var scale = map.getScale();
+              var scaledViewport = e.data[0];
+              var smallerScaledViewport = e.data[1];
+              var containersUnderChangedArea = [];
+              var isOutside, scaledAndChangedViewport;
+
+              scaledAndChangedViewport = Object.assign({}, scaledViewport);
+
+              scaledAndChangedViewport.width += Math.round(Math.abs(changedCoordinates.width));
+              scaledAndChangedViewport.height += Math.round(Math.abs(changedCoordinates.height));
+
+              /* RESET */
+              changedCoordinates.width = 0;
+              changedCoordinates.height = 0;
+
+              containersUnderChangedArea = map.getMovableLayer().getPrimaryLayers().map(layer => {
+                return layer.getSubContainersByCoordinates(scaledAndChangedViewport);
+              });
+              containersUnderChangedArea = arrays.flatten2Levels(containersUnderChangedArea);
+
+              containersUnderChangedArea.forEach((thisContainer) => {
+                isOutside = isObjectOutsideViewport(thisContainer, smallerScaledViewport, true, scale);
+
+                thisContainer.visible = isOutside ? false : true;
+                // I do not know is caching between the moves better or not-caching.
+                // thisContainer.setCache(false);
+                // thisContainer.setCache(true);
+              });
+
+              queue.processing = false;
+
+              map.drawOnNextTick();
+            } catch (ev) {
+              console.log("ERROR", ev);
+            }
+          };
+          viewportWorker.postMessage([
+            1,
+            viewportArea,
+            map.getScale(),
+            changedCoordinates
+          ]);
+        } else {
+          queue.processing = false;
+          throw new Error("ERROR WITH WEB WORKER");
+        }
+      } catch (e) {
+        queue.processing = false;
+        console.log(e);
+      }
+    }
+
+    return;
+  }
+  function startEventListeners(map) {
+    mapEvents.subscribe("mapMoved", moveCb);
+    mapEvents.subscribe("mapResized", resizeCb);
+
+    function moveCb(type, movedCoordinates) {
+      changedCoordinates.width += movedCoordinates.x;
+      changedCoordinates.height += movedCoordinates.y;
+      managingTileMapMovement.check(map);
+    }
+    function resizeCb() {
+      managingTileMapMovement.check(map);
+    }
+  }
+  /**************************************
+  *********** PRIVATE *******************
+  **************************************/
+  function isObjectOutsideViewport(object, viewportArea, hasParent = true, scale = 1) {
+    var isIt, globalCoords;
+
+    globalCoords = object.getSubcontainerArea(scale, { toGlobal: hasParent });
+
+    isIt = !testRectangleIntersect(globalCoords, viewportArea);
+
+    return isIt;
+  }
+  function getViewportsRightSideCoordinates(viewportArea) {
+    var offsetSize = Math.abs( viewportArea.width * VIEWPORT_OFFSET * 2 );
+
+    return {
+      x2: Math.round( viewportArea.x + Math.abs( viewportArea.width ) + offsetSize ),
+      y2: Math.round( viewportArea.y + Math.abs( viewportArea.height ) + offsetSize ),
+      width: Math.round( viewportArea.width + offsetSize ),
+      height: Math.round( viewportArea.height + offsetSize )
+    };
+  }
+  function applyScaleToViewport(viewportArea, scale) {
+    return {
+      x: Math.round( viewportArea.x / scale ),
+      y: Math.round( viewportArea.y / scale ),
+      x2: Math.round( viewportArea.x2 / scale ),
+      y2: Math.round( viewportArea.y2 / scale ),
+      width: Math.round( viewportArea.width / scale ),
+      height: Math.round( viewportArea.height / scale )
+    };
+  }
+}
+/***********************
+******* PRIVATE ********
+***********************/
+function testRectangleIntersect(a, b) {
+  return (a.x <= b.x + b.width &&
+          b.x <= a.x + a.width &&
+          a.y <= b.y + b.height &&
+          b.y <= a.y + a.height);
+}
---
+++ public/components/map/extensions/mapMovement/managingGeneralWorker.js
@@ -0,0 +1,83 @@
+/* global self, postMessage */
+
+'use strict';
+
+/**
+ * This webworker receives array as postMessage. The first index will determine what we do. First index is a value of
+ * integer.
+ */
+
+/* object.assign polyfill for IE11 */
+if (typeof Object.assign != 'function') {
+  (function () {
+    Object.assign = function (target) {
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var output = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var source = arguments[index];
+        if (source !== undefined && source !== null) {
+          for (var nextKey in source) {
+            if (source.hasOwnProperty(nextKey)) {
+              output[nextKey] = source[nextKey];
+            }
+          }
+        }
+      }
+      return output;
+    };
+  })();
+}
+
+const VIEWPORT_OFFSET = 0.1;
+const METHOD_ALL = 1;
+const METHOD_SHORT = 0;
+var viewportArea, scale, methodType, smallerViewportArea;
+
+self.addEventListener("message", function (e) {
+  var scaledViewport, smallerScaledViewportArea;
+
+  methodType = e.data[0];
+  viewportArea = e.data[1];
+  scale = e.data[2];
+
+  if (methodType === METHOD_ALL) {
+    try {
+      smallerViewportArea = Object.assign( {}, getViewportCoordinates(viewportArea, 0.5));
+      Object.assign( viewportArea, getViewportCoordinates(viewportArea));
+
+      scaledViewport = Object.assign({} , applyScaleToViewport(viewportArea, scale) );
+      smallerScaledViewportArea = Object.assign({} , applyScaleToViewport(smallerViewportArea, scale) );
+
+      postMessage([scaledViewport, smallerScaledViewportArea]);
+    } catch (ev) {
+      console.log("ISSUE: ", ev);
+    }
+  }
+});
+
+function getViewportCoordinates(viewportArea, offsetQuantifier) {
+  var offsetSize = Math.abs( viewportArea.width * VIEWPORT_OFFSET  );
+  offsetQuantifier = offsetQuantifier || 1;
+
+  return {
+    x: Math.round( viewportArea.x - offsetSize * offsetQuantifier ),
+    y: Math.round( viewportArea.y - offsetSize * offsetQuantifier ),
+    x2: Math.round( viewportArea.x + Math.abs( viewportArea.width ) + offsetSize * offsetQuantifier ),
+    y2: Math.round( viewportArea.y + Math.abs( viewportArea.height ) + offsetSize * offsetQuantifier ),
+    width: Math.round( viewportArea.width + offsetSize * 2 * offsetQuantifier ),
+    height: Math.round( viewportArea.height + offsetSize * 2 * offsetQuantifier )
+  };
+}
+function applyScaleToViewport(viewportArea, scale) {
+  return {
+    x: Math.round( viewportArea.x / scale ),
+    y: Math.round( viewportArea.y / scale ),
+    x2: Math.round( viewportArea.x2 / scale ),
+    y2: Math.round( viewportArea.y2 / scale ),
+    width: Math.round( viewportArea.width / scale ),
+    height: Math.round( viewportArea.height / scale )
+  };
+}

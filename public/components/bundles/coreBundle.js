a3a0490d796d0a8e546578d0c1dd36d9a26abf1e
Janne Hyytiä <janne.hyytia@saunalahti.fi>
2016-01-30 20:34:08 +0200

    Fixed bugs in bundles

--- public/components/bundles/coreBundle.js
+++ public/components/bundles/coreBundle.js
@@ -7,7 +7,7 @@
 
 export * from '/components/map/core/utils/index';
 export * from '/components/map/core/Objects';
-export * from '/components/map/core/eventListeners';
+export * from '/components/map/core/baseEventlisteners/baseEventListeners';
 export * from '/components/map/core/Map';
 export * from '/components/map/core/MapDataManipulator';
 export * from '/components/map/core/MapLayers';

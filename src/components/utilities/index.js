031d8fefa4db021ebd08898018a7a8b128ecf2d6
Janne Hyytiä <janne.hyytia@saunalahti.fi>
2016-02-09 21:24:14 +0200

    Reference commit: d127b1d66dd659a90565947ac4ce332a8be6f7a6 forgot 2 files from the commit

---
+++ src/components/utilities/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* Bundle utils in utils-parent and export them */
+import { polyfills } from './polyfills';
+
+export const generalUtils = {
+	polyfills
+};
---
+++ src/factories/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* Bundle utils in utils-parent and export them */
+import { createHorizontalHexagonMap } from './horizontalHexaFactory';
+
+export const factories = {
+  createHorizontalHexagonMap
+};
